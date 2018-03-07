2579ff86da4804830ebfcf87b55cfd4577b85322
Alex Zdanowicz <alex.zdanowicz@gmail.com>
2018-03-07 00:22:04 -0500

    Added some basic functions to the TextInterface

--- src/com/company/view/text/TextInterface.java
+++ src/com/company/view/text/TextInterface.java
@@ -1,6 +1,20 @@
 package com.company.view.text;
 
+/**
+ * TODO: Write documentation
+ */
 public interface TextInterface {
 
+    /**
+     * TODO: Write documentation
+     * @return
+     */
+    public abstract String getText();
+
+    /**
+     * TODO: Write documentation
+     * @param text
+     */
+    public abstract void setText(String text);
 
 }//end TextInterface
