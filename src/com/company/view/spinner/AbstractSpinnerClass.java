310ad763950c99963bfb88811e55d5738f07cdac
Alex Zdanowicz <alex.zdanowicz@gmail.com>
2022-11-06 21:59:53 +1000

    First part of creating the spinner class

---
+++ src/com/company/view/spinner/AbstractSpinnerClass.java
@@ -0,0 +1,18 @@
+package com.company.view.spinner;
+
+import javax.swing.*;
+
+/**
+ * The generic spinner class for all spinner class
+ */
+public abstract class AbstractSpinnerClass implements SpinnerInterface {
+
+    /* ************ *
+     *    Fields    *
+     * ************ */
+
+    /* Variables */
+
+    private JLabel spinnerHolder;
+
+}
---
+++ src/com/company/view/spinner/SpinnerInterface.java
@@ -0,0 +1,17 @@
+package com.company.view.spinner;
+
+/**
+ * The generic interface for all spinners
+ */
+public interface SpinnerInterface {
+
+    /**
+     * Turns the spinner "on"
+     */
+    void displaySpinner();
+
+    /**
+     * Turns the spinner "off"
+     */
+    void hideSpinner();
+}
